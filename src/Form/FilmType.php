2bb3d89c21770299a568b9b849815e42c3bd957c
fidel reyes <freyesllerena33@gmail.com>
2018-03-03 16:39:06 +0100

    add form film

--- src/Form/FilmType.php
+++ src/Form/FilmType.php
@@ -2,11 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Film;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class FilmsType extends AbstractType
+class FilmType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -18,6 +19,8 @@ class FilmsType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
+            // uncomment if you want to bind to a class
+            //'data_class' => Film::class,
         ]);
     }
 }
